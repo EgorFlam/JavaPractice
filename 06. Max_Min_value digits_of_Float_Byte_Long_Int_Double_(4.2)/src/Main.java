bb4e4d2cb336b03ae932cb15713c9255ae0b5f8d
Egor FLAM <egorvereschagin@gmail.com>
2022-06-08 23:27:43 +0300

    added folder: 06. Max_Min_value digits_of_Float_Byte_Long_Int_Double_(4.2)

---
+++ 06. Max_Min_value digits_of_Float_Byte_Long_Int_Double_(4.2)/src/Main.java
@@ -0,0 +1,22 @@
+public class Main
+{
+    public static void main(String[] args) {
+        System.out.println("integerMAX: " + Integer.MAX_VALUE);
+        System.out.println("integerMIN: " + Integer.MIN_VALUE);
+
+        System.out.println("doubleMAX: " + Double.MAX_VALUE);
+        System.out.println("doubleMIN: " + Double.MIN_VALUE);
+
+        System.out.println("shortMAX: " + Short.MAX_VALUE);
+        System.out.println("shortMIN: " + Short.MIN_VALUE);
+
+        System.out.println("byteMAX: " + Byte.MAX_VALUE);
+        System.out.println("byteMIN: " + Byte.MIN_VALUE);
+
+        System.out.println("longMAX: " + Long.MAX_VALUE);
+        System.out.println("longMIN: " + Long.MIN_VALUE);
+
+        System.out.println("floatMAX: " + Float.MAX_VALUE);
+        System.out.println("floatMIN: " + Float.MIN_VALUE);
+    }
+}
