400948e191fdf9f6148791755fdbde25cf6b9f4b
Egor FLAM <egorvereschagin@gmail.com>
2022-06-08 22:25:57 +0300

    Integer method has been updated

--- 05. IntegerExperiments/src/Container.java
+++ 05. IntegerExperiments/src/Container.java
@@ -1,4 +1,22 @@
 public class Container
 {
     public int count;
+
+    public Integer sumDigits(Integer number)
+    {
+        /*
+     статический метод объекта Integer.parseInt(String string)
+     преобразует объект типа String в объект типа Integer
+     т.е. преобразовываем числа из формата "текст" в формат "число", в противном случае, мы не сможем
+     производить с ними арифметические действия
+     если формат строки невозможно интерпретировать как число - получим ошибку (java.lang.NumberFormatException)
+     */
+        //@TODO: write code here
+        String s = Integer.toString(number);
+        Integer sum = number;
+        for (int i = 0; i < s.length(); i++) {
+            sum += Integer.parseInt(String.valueOf(s.charAt(i)));
+        }
+        return sum;
+    }
 }
--- 05. IntegerExperiments/src/Main.java
+++ 05. IntegerExperiments/src/Main.java
@@ -9,25 +9,11 @@ public class Main
         container.count += 7843;
 
         System.out.println(container.count);
+
+        System.out.println(container.sumDigits(12));
     }
 
-    public Integer sumDigits(Integer number)
-    {
-        /*
-     статический метод объекта Integer.parseInt(String string)
-     преобразует объект типа String в объект типа Integer
-     т.е. преобразовываем числа из формата "текст" в формат "число", в противном случае, мы не сможем
-     производить с ними арифметические действия
-     если формат строки невозможно интерпретировать как число - получим ошибку (java.lang.NumberFormatException)
-     */
-        //@TODO: write code here
-       String s = Integer.toString(number);
-       Integer sum = 12345;
-       for (int i = 0; i < s.length(); i++) {
-           sum += Integer.parseInt(String.valueOf(s.charAt(i)));
-       }
-        return sum;
-    }
+
 
 }
 
