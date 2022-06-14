60645278574c52b0cd0af297e509fe6bc77d6cff
Egor FLAM <egorvereschagin@gmail.com>
2022-06-14 22:58:49 +0300

    Updated GeometryCalculator and IntegerExpirements codes

---
+++ 07.GeometryCalculator_Task_4.3/Math'sPractice/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+//        for (int i = 0; i <= 10; i++)
+//        {
+//            System.out.println(i);
+//        }
+//        System.out.println("===============================");
+//        for (int i = 10; i >= 0; i--)
+//        {
+//            System.out.println(i);
+//        }
+        double value = 2.7;
+        int roundedValue = (int) Math.round(value);
+        System.out.println(roundedValue);
+
+        System.out.println(Math.sqrt(9));
+        System.out.println(Math.pow(9, 0.5));
+
+
+    }
+}
